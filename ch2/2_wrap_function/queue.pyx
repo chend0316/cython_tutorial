01a37d7b1a402f8b488c8eabacea135f9b26bc8d
chend <chend@ruijie.com.cn>
2019-03-05 11:28:17 +0800

    补充ch2 wrap function

--- ch2/2_wrap_function/queue.pyx
+++ ch2/2_wrap_function/queue.pyx
@@ -1,5 +1,7 @@
 #file: queue.pyx
 
+from cpython.ref cimport PyObject
+
 cdef extern from "queue.h":
     ctypedef struct Queue:
         pass
@@ -7,6 +9,8 @@ cdef extern from "queue.h":
 
     Queue *queue_new()
     void queue_free(Queue *queue)
+    int queue_push_tail(Queue *queue, QueueValue data)
+    QueueValue queue_pop_tail(Queue *queue)
 
 def foo():
     # 虽然没有实际意义，但这段代码很自嗨，可以看到Cython中完全可以调用C函数
@@ -19,7 +23,25 @@ cdef class PyQueue:
 
     def __cinit__(self):
         self._c_queue = queue_new()
+        if self._c_queue == NULL:
+            raise MemoryError()
 
     def __dealloc__(self):
         if self._c_queue is not NULL:
             queue_free(self._c_queue)
+
+    def push_tail(self, v):
+        if (queue_push_tail(self._c_queue, <void*>v) != 0):
+            (<PyObject*>v).ob_refcnt += 1
+        else:
+            raise MemoryError()
+
+    def pop_tail(self):
+        cdef QueueValue v
+        v = queue_pop_tail(self._c_queue)
+        if v != NULL:
+            obj = <object>v
+            (<PyObject*>obj).ob_refcnt -= 1
+            return obj
+        else:
+            return None
--- ch2/2_wrap_function/test.py
+++ ch2/2_wrap_function/test.py
@@ -1,3 +1,9 @@
 import queue
 
 q = queue.PyQueue()
+q.push_tail('a')
+q.push_tail(123)
+q.push_tail({'name': 'zhang', 'age': 18})
+print(q.pop_tail())
+print(q.pop_tail())
+print(q.pop_tail())
